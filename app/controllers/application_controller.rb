8a4253bd3281baaca6b0ea719b8a154e05e33896
aburk3 <aburk3@gmail.com>
2018-12-09 15:44:17 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(params[:id])
+    erb :show
+  end
 
 end
