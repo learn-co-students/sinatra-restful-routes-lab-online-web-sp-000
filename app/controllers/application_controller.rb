0f343ef57dca4275912e2a28c664325614ff40b2
aburk3 <aburk3@gmail.com>
2018-12-10 06:16:38 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,8 +23,19 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  patch '/recipes/:id' do  #updates a recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
   post '/recipes' do
     @recipe = Recipe.create(params)
     redirect "/recipes/#{@recipe.id}"
   end
+
+  
 end
