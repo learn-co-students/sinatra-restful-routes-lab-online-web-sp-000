a080cf6e551f430f3b460ca15e39f63420aabc0b
Jessica Gladstone <jcg321@comcast.net>
2019-02-22 01:55:18 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   
   
   post '/recipes' do
-    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cooking_time => params[:cooking_time])
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect '/recipes/#{@recipe.id}'
   end
   
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
-    @recipe.cooking_time = params[:cooking_time]
+    @recipe.cook_time = params[:cook_time]
     @recipe.save
     redirect '/recipes/#{@recipe.id}'
   end
