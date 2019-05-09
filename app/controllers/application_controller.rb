e7718fb99edf3284ffc2c31eb527908ccaa99997
John Hayes <johnnyhayes7@gmail.com>
2019-05-09 13:50:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+# require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,17 +6,60 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
   get '/recipes' do
     @recipes = Recipe.all
   
     erb :index
   end
   
-  get '/recipes/:id' do
+  get '/recipes/new' do
     
-    @recipe = Recipe.all.find_by(params[:id])
-    # binding.pry
+    erb :new
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    
+    erb :edit
+  end
+  
+  
+  get '/recipes/:id' do
+     @recipe = Recipe.find(params[:id])
+    
     erb :show
   end
+  
+  
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+   
+    redirect "/recipes/#{@recipe.id}"
+    
+  end
+  
+  
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(params[:id])
+    # binding.pry
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    
+    
+     redirect "/recipes/#{@recipe.id}"
+  end
+  
+  
+  delete '/recipes/:id' do
+    recipe = Recipe.find_by(params[:id])
+    recipe.destroy
+    binding.pry
+    redirect '/recipes'
+  end
+  
+  
 
 end
