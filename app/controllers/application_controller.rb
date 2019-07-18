31167ee2a563473829690fe22271fd57f5b52b57
Ramon Chavez <rachavez1974@gmail.com>
2019-07-17 21:51:48 -0400

    Add new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
