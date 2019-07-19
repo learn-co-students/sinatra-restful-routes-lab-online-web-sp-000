6ccaf8cf31431f5f9acff35b2d75637b31091c5a
Alan Krajina <alankrajina88@gmail.com>
2019-07-19 14:04:35 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,18 +1,4 @@
-class Recipe
-  attr_accessor :name, :ingredients, :cook_time
- 
- RECIPES = []
- 
-  def initialize(params)
-    @name = params[:name]
-    @ingredients = params[:ingredients]
-    @cook_time = params[:cook_time]
-    RECIPES << self
-    
-  end
- 
-  def self.all
-    RECIPES
-  end
+class Recipe < ActiveRecord::Base
+
   
 end
