f678938eff18088ba054e9b15bcd269e19b724de
Alan Krajina <alankrajina88@gmail.com>
2019-07-19 14:01:35 +0000

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,18 @@
-#Placeholder for a model
+class Recipe
+  attr_accessor :name, :ingredients, :cook_time
+ 
+ RECIPES = []
+ 
+  def initialize(params)
+    @name = params[:name]
+    @ingredients = params[:ingredients]
+    @cook_time = params[:cook_time]
+    RECIPES << self
+    
+  end
+ 
+  def self.all
+    RECIPES
+  end
+  
+end
