0b730a054e187f42b3f93ea6cf75831ae4dc4446
Yingqi Chen <chanwk18@hotmail.com>
2019-08-16 19:58:25 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -9,6 +11,42 @@ class ApplicationController < Sinatra::Base
     @all=Recipe.all
     erb :index
   end
-end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe=Recipe.create(name: params[:name],ingredients: params[:ingredients],cook_time: params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @id=params[:id]
+    @recipe=Recipe.find_by_id(@id)
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe=Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe=Recipe.find_by_id(params[:id])
+    @recipe.name=params[:name]
+    @recipe.cook_time=params[:cook_time]
+    @recipe.ingredients=params[:ingredients]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    @recipe=Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
+
+
 
 end
