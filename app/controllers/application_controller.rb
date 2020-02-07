4e662ec21e9b5e56f9883575b3261aebcb47bbda
David Strauch <dmaster18@gmail.com>
2020-02-07 15:36:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,25 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  get "/recipes" do 
+    @recipes = Recipe.all
+    erb :index
+  end
+  
+  get "/recipes/new" do 
+    erb :new
+  end
+  
+  
+  post "/recipes" do 
+    name = params[:name]
+    ingredients = params[:ingredients]
+    cook_time = params[:cook_time].to_i
+    @recipe = Recipe.new(name: name, ingredients: ingredients, cook_time: cook_time)
+    @recipe.save
+    redirect "/recipes/"
+  end
+  
 
 end
