915d1c7380420721d55197704df76d29f1bcdaa8
Anthony Tilelli <anthony@tilelli.me>
2020-02-14 01:01:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,33 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # # code actions here!
+  # get '/recipes' do
+  #   erb :index
+  # end
+  
+  # Index page '/recipes'
+  get  '/recipes' do
+  end
+  
+  post  '/recipes' do
+  end
+  
+  # new page '/recipes/new'
+  get '/recipes/new' do
+  end
+  
+  # Show page '/recipes/:id'
+  get '/recipes/:id' do
+  end
+  
+  patch '/recipes/:id' do
+  end
+  
+  delete '/recipes/:id' do
+  end
 
+  # edit page '/recipes/:id/edit'
+  get '/recipes/:id/edit' do
+  end
 end
