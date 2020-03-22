d91ccf61c5010f84a780f6ec9c7ddb050b906b3c
Michael Carroll <carrollm2@gmail.com>
2020-03-22 01:32:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  post '/articles' do
+  post '/recipes' do
 
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
 
