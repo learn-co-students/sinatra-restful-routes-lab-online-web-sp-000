79f28895718e14773ac70b7b55e20ee0b8018049
LostnAustin <chadlengefeld@gmail.com>
2020-05-14 23:18:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,36 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/' do
+    @recipes = Recipe.all
+    erb :recipes
+  end
+  
+  get '/recipes/new' do
+    erb :new
+  end
+
+
+  get '/recipes' do 
+    @recipes = Recipe.all
+    #  binding.pry
+    erb :recipes
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/show' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
 
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,14 @@
 #Placeholder for a model
+class Recipe < ActiveRecord::Base
+    # attr_reader :name, :ingredients
+    # RECIPES = []
+    # def initialize(recipe)
+    #     @name = recipe[:name]
+    #     @ingredients = recipe[:ingredients]
+    # end
+
+    # def self.all
+    # RECIPES
+    # end
+
+end
