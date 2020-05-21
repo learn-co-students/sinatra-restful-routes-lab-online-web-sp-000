90d6a15a0cfb8ea615f7e99653619ae3439ff6a7
bruffin1221 <bruffin1221@gmail.com>
2020-05-21 00:00:35 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,48 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+get '/' do 
+end 
+
+get '/recipes/new' do
+  erb :new
+end
+
+post '/recipes' do
+@recipes=Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+@recipes.name=params[:name]
+@recipes.ingredients=params[:ingredient]
+@recipes.cook_time=params[:cook_time]
+redirect to "/recipes/#{@recipes.id}"
+end
+
+get '/recipes' do 
+  @recipes=Recipe.all
+  erb :index
+end
+
+get '/recipes/:id' do 
+  @recipes=Recipe.find_by_id(params[:id])
+  erb :show
+end
+
+get '/recipes/:id/edit' do
+  @recipes=Recipe.find_by_id(params[:id])
+  erb :edit
+end
+
+patch '/recipes/:id' do
+  @recipes=Recipe.find(params[:id])
+  @recipes.update(params[:recipe])
+  @recipes.save
+  redirect "/recipes/#{@recipes.id}"
+ end
+ 
+ delete '/recipes/:id' do
+  @recipes=Recipe.find_by_id(params[:id])
+  @recipes.destroy
+  redirect '/recipes'
+ end
+
+
 end
