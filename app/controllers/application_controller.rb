18bcbd90eb69be3d116198ca214a5b4fd7a08ce8
chienleow <chienleow1@gmail.com>
2020-06-17 18:03:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,29 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do
+    # create and save this new recipe to database
+
+  end
+
+  get '/recipes/:id' do
+    # display a single recipe
+  end
+
+  get '/recipes/:id/edit' do
+    # update entry in the database
+    # then redirect to the recipe show page
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    # binding.pry
+    erb :index
+  end
+
+  delete '/recipes/:id' do
+    #deletes the entry from database
+    # redicts to the index page
+  end
 
 end
