801aa8c7e4f064bc7aae30b173d446a6c0cd45e2
chienleow <chienleow1@gmail.com>
2020-06-18 14:24:29 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,33 +5,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
-    # create and save this new recipe to database
+    erb :new
+  end
 
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
-    
     erb :show
   end
 
   get '/recipes/:id/edit' do
-    # update entry in the database
-    # then redirect to the recipe show page
     @recipe = Recipe.find_by_id(params[:id])
-
     erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes' do
     @recipes = Recipe.all
-    
     erb :index
   end
 
   delete '/recipes/:id' do
-    #deletes the entry from database
-    # redicts to the index page
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
   end
 
 end
