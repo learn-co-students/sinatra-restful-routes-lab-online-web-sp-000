30d505ace2302b8fe7b1bc069c135c46f1d52db4
milos381 <milos_popovic@outlook.com>
2020-09-29 23:42:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,54 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/recipes/new' do
+  get '/recipes/new' do # loads form to create new recipe
     erb :new
   end
 
-  post '/recipes' do 
-    recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients])
+  post '/recipes' do # create action creating and saving new recipe based on params from form, then redirects to show page
+    @recipe = Recipe.create(params)
+
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  get 'recipes/:id' do
+  get '/recipes/:id' do # show action to display a single recipe. dynamic url allows ID to be called in the view thru params hash
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
-  get '/recipes/:id/edit' do
+  get '/recipes/:id/edit' do #load edit form
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
 
-  patch 'recipes/:id' do
+  patch '/recipes/:id' do #edit action. edit form submission. redirect to show page.
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
+
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  get '/recipes' do
-    @recipe = Recipe.all
+  get '/recipes' do # index action allowing view to access all recipes thru instance variable @recipes
+    @recipes = Recipe.all
     erb :index
   end
 
  
 
-  delete '/recipes/:id' do
+  delete '/recipes/:id' do #delete action
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.delete
+
     redirect to '/recipes'
   end
   
