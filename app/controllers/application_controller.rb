03129c9f7e6a41d87c3d78c27b20cc6feae8083b
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-10-02 17:20:41 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  #CREATE
+
+    # new action
+    get '/recipes/new' do
+      erb :new
+    end
+
+    post '/recipes' do
+      recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect "/recipes/#{recipe.id}"
+    end
+
+  #READ
+
+    get '/' do
+      redirect '/recipes'
+    end
+
+    # index action
+    get '/recipes' do
+      @recipes = Recipe.all
+      erb :index
+    end
+
+    # show action
+    get '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      erb :show
+    end
+
+  #UPDATE
+
+    # edit action
+    get '/recipes/:id/edit' do
+      @recipe = Recipe.find(params[:id])
+      erb :edit
+    end
+
+    patch '/recipes/:id' do
+      recipe = Recipe.find(params[:id])
+      recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect "/recipes/#{recipe.id}"
+    end
+
+  #DELETE
+
+    # delete action
+    delete '/recipes/:id' do
+      Recipe.destroy(params[:id])
+      redirect "/recipes"
+    end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Recipe.find_or_create_by(name: "Lasagna", ingredients: "cheese, pasta layers, tomatoes, sauce", cook_time: "40 min.")
+Recipe.find_or_create_by(name: "Salad", ingredients: "arugula, watermelon", cook_time: "10 min.")
+Recipe.find_or_create_by(name: "Sangria", ingredients: "port wine, sprite, ice, oranges, apples", cook_time: "10 min.")
