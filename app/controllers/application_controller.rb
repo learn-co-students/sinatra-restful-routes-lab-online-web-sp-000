eec57e78ed1423882ded307fbc99b12c356fe919
jrog20 <jaimerogstad@gmail.com>
2020-10-23 18:19:34 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,21 +14,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/recipes' do
-    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe = Recipe.new(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save
     redirect to "/recipes/#{@recipe.id}"
   end
 
-  get 'recipes/:id' do
+  get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :edit
-  end
-
-  post '/recipes/:id' do
     erb :edit
   end
  
