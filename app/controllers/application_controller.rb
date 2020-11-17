73eb41231046770b31cbfb2314840123562de525
concow <ccowan003@comcast.net>
2020-11-17 15:52:53 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,17 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes/new' do
+    erb :new            #renders form to create new recipe
+  end
 
+  #This controller action should create and save this new recipe to the database.
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes' do
+    erb :recipes
+  end
 end
