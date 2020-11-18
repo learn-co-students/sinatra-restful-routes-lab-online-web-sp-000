9efff82acb9938f9686d5fad29aadc231cbf7299
concow <ccowan003@comcast.net>
2020-11-18 15:40:59 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,31 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  #renders form to create new recipe, should create and save this new recipe to the database.
   get '/recipes/new' do
-    erb :new            #renders form to create new recipe
+    erb :new
   end
 
-  #This controller action should create and save this new recipe to the database.
+  #RESTful routes to display a single recipe.
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect "/recipes/#{@recipe.id}"
   end
 
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  #RESTful routes and renders a form to edit a single recipe
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  #(index action) that displays all the recipes in the database.
   get '/recipes' do
     erb :recipes
   end
+
 end
