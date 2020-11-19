a0b162b2d7f203a8367269fa6d6fbc726d9bb754
concow <ccowan003@comcast.net>
2020-11-18 19:07:17 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,26 +10,43 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  #RESTful routes to display a single recipe.
-  post '/recipes' do
+  post '/recipes' do    #create action
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    redirect "/recipes/#{@recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
+  #show action, display a single recipe
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
 
-  #RESTful routes and renders a form to edit a single recipe
-  get '/recipes/:id/edit' do
+  #renders a form to edit a single recipe
+  get '/recipes/:id/edit' do     #load edit form
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
+  end
+
+  #update the entry in the database with the changes
+  patch '/recipes/:id' do   #edit action
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   #(index action) that displays all the recipes in the database.
   get '/recipes' do
     erb :recipes
+  end
+
+  #Add to the recipe show page a form that allows a user to delete a recipe
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
   end
 
 end
