5b3dab2328268829c5b6bedb81f31df2fb9edaeb
Christerle Nounoute <christerlesn@gmail.com>
2021-02-17 03:56:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+get '/recipes' do
+  @recipes = Recipe.all
+  erb :index
+end
+
+get '/recipes/:id' do
+  @recipe = Recipe.find_by(params[:id])
+  erb :show
+  #display's recipe's name, ingredients, and cook_time
+  #has a delete button
+end
+
+get '/recipes/:id/edit' do
+
+end
+#not sure if I need the patch one
+patch '/recipes/:id' do
+
+end
+
+get '/recipes/new' do
+
+end
+
   # code actions here!
 
 end
