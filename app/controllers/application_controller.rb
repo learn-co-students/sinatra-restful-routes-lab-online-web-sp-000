d6f8542894affdccee13d0bfbd4b58c9f101dfe9
jrrohrer <jessie.rohrer@gmail.com>
2021-02-25 17:37:39 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  #create new recipe & save to database
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    @recipe.save  
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  #display a single recipe
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  #renders a form to edit a single recipe. Update the entry in the database with the changes, and then reidrect to the recipe show page.
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  #index action that shows all the recipes in the database; includes a form that allows the user to delete a recipe.
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  #delete action that removes the entry from the database and redirects to the index page.
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+
 
 end
