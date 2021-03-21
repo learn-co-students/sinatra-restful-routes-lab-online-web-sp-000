a35cd235cfb2cea708a192af4a5d6bcdb60fc427
Jess Pesale <jess_pesale@aol.com>
2021-03-21 18:01:16 -0400

    Done.

---
+++ app/views/edit.rb
@@ -0,0 +1,20 @@
+<form method="post" action="/recipes/<%=@recipe.id %>">
+    <input id="hidden" type="text" name="_method" value="patch">
+    <input type="text" name="name">
+    <input type="text" name="ingredients">
+    <input type="text" name="cook_time">
+
+    <input type="submit" value="submit">
+</form>
+
+
+
+
+
+# <form action="/recipes/<%= @recipe.id %>" method="post">
+#     <input id="hidden" type="hidden" name="_method" value="patch">
+#     <input type="text" name="name">
+#     <input type="text" name="ingredients">
+#     <input type="text" name="cook_time">
+#     <input type="submit" value="submit">
+# </form>
